64be0058495b3c35736d8b09ca850146ffcb5215
dungnvkms <dungvannguyen@kms-technology,com>
2022-03-06 22:45:01 +0700

    update init

--- src/main/java/com/example/reactiveprogramming/demo/Demo2Unsubscribe.java
+++ src/main/java/com/example/reactiveprogramming/demo/Demo2Unsubscribe.java
@@ -5,10 +5,15 @@ import rx.Subscriber;
 
 public class Demo2Unsubscribe {
     public static void main(String[] args) {
-        Observable<Integer> observable = Observable.just(10, 20, 30)
+        Observable<Integer> observable = Observable.just(10, 20, 30, 40)
                 .doOnNext(s -> System.out.println("map "+ s))
                 .doOnCompleted(() -> System.out.println("complete"));
         observable.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onStart() {
+                request(2);
+            }
+
             @Override
             public void onCompleted() {
 
@@ -22,7 +27,7 @@ public class Demo2Unsubscribe {
             @Override
             public void onNext(Integer integer) {
                 System.out.println(integer);
-                if (integer == 20) unsubscribe();
+                if (integer == 30) unsubscribe();
             }
         });
     }
